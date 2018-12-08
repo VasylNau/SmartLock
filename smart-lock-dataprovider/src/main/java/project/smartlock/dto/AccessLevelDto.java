bd2b910e0deb31f1aa7f1ea3e6c68690f9b45c9a
Vasyl Naulychnyi <vasy1nauly4nyi@gmail.com>
2018-12-08 18:03:35 +0200

    add dto classes

---
+++ smart-lock-dataprovider/src/main/java/project/smartlock/dto/AccessLevelDto.java
@@ -0,0 +1,12 @@
+package project.smartlock.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccessLevelDto {
+
+    private Integer id;
+    private String name;
+
+}
---
+++ smart-lock-dataprovider/src/main/java/project/smartlock/dto/LockDto.java
@@ -0,0 +1,22 @@
+package project.smartlock.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class LockDto {
+
+    private Long id;
+
+    private String name;
+    private String description;
+
+    private Boolean active;
+    private Boolean deleted;
+
+    private List<UnlockingHistoryItemDto> unlockingHistoryItems;
+    private List<UserToLockRelationDto> relatedUsers;
+}
